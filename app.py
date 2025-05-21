b6bf12c698a0e8a5e7b0227cf0fab357a66dc781
joonsol <71648638+joonsol@users.noreply.github.com>
2025-05-21 06:42:28 +0000

    2

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 st.title("🎓 학생 점수 분석기")
-st.title("🎓 학생 점수 분석기")
-st.text("🎓 학생 점수 분석기")
+
+if "names" not in st.session_state:
+    st.session_state.names=[]
+    st.session_state.scores=[]
+
+name=st.text_input("이름입력")
